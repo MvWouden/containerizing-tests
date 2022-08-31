5745cd1f00f90dbee95a5c3f2de328b1d435fc2a
Milan van Wouden <milanvanwouden@live.nl>
2022-08-31 13:18:35 +0200

    strip project

--- vue-node/client/tests/unit/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import HomeView from '@/views/HomeView.vue';
-
-describe('HelloWorld.vue', () => {
-  it('renders "Hello, World"', () => {
-    const wrapper = shallowMount(HomeView);
-    expect(wrapper.text()).toMatch('Hello, World!');
-  });
-});
