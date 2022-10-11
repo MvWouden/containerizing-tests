4bb676ddfbed49ac1cb2757c5cd4d469bcb78ef9
Milan van Wouden <milanvanwouden@live.nl>
2022-10-11 19:49:19 +0200

    using generics in class-based views

--- django-postgres-rest/templateapp/views.py
+++ django-postgres-rest/templateapp/views.py
@@ -1,33 +1,14 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import (
-    CreateModelMixin, DestroyModelMixin, ListModelMixin,
-    RetrieveModelMixin, UpdateModelMixin
-)
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from templateapp.models import ToDo
 from templateapp.serializers import ToDoSerializer
 
 
-class ToDoList(ListModelMixin, CreateModelMixin, GenericAPIView):
+class ToDoList(ListCreateAPIView):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ToDoDetail(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
+class ToDoDetail(RetrieveUpdateDestroyAPIView):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
